a27faff37163245fedcd8cb86672c22a784b5f47
Bharat <hibharatj@gmail.com>
2025-06-23 16:22:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
-    //Write your code here
+    event.preventDefault(); 
 
+    const form = document.getElementById("form1");
+
+    const firstName = form.fname.value.trim();
+    const lastName = form.lname.value.trim();
+
+    alert(firstName + " " + lastName);
 }
